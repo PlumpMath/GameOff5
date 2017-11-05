8561c73905a12614a98584f535c8b5b190e12247
Your3Wishes <guitarfreak993@yahoo.com>
2017-11-05 10:01:33 -0600

    Added Simple Grab and Throw

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    public float jumpForce = 200;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float moveSpeed = 10;
+
+    private bool onGround = true;
+    private LayerMask groundLayerMask;
+    private bool facingRight = true;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        groundLayerMask = LayerMask.GetMask("Ground");
+    }
+	
+	void Update () {
+        applyFallMultiplier();
+        checkForFlip();
+    }
+
+    void FixedUpdate()
+    {
+        moveHorizontal();
+        checkForJump();
+        checkIfGrounded();
+ 
+    }
+
+    void moveHorizontal()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal != 0)
+            rigidBody.velocity = (new Vector2(moveHorizontal * moveSpeed, rigidBody.velocity.y));
+    }
+
+    void checkForJump()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (onGround)
+            {
+                Jump();
+                onGround = false;
+            }
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.AddForce(new Vector2(0.0f, jumpForce));
+    }
+
+    void checkIfGrounded()
+    {
+        if (rigidBody.velocity.y <= 0)
+        {
+            RaycastHit2D hit2D = Physics2D.Raycast(rigidBody.position, Vector2.down, 1.2f, groundLayerMask);
+            if (hit2D)
+            {
+                onGround = true;
+            }
+            else
+            {
+                onGround = false;
+            }
+        }
+    }
+
+    void applyFallMultiplier()
+    {
+        if (rigidBody.velocity.y < 0)
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidBody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void checkForFlip()
+    {
+        if (rigidBody.velocity.x > 0 && !facingRight || rigidBody.velocity.x < 0 && facingRight)
+        {
+            flipPlayer();
+        }
+    }
+
+    void flipPlayer()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
+}
---
+++ Assets/Scripts/PlayerGrabItem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabItem : MonoBehaviour {
+
+    public float distance = 2f;
+    public float throwForce = 2f;
+    public Transform grabPoint;
+
+    private RaycastHit2D hit;
+    private bool hasItem = false;
+    private LayerMask grabLayerMask;
+
+
+    void Start ()
+    {
+        grabLayerMask = LayerMask.GetMask("Grab");
+    }
+	
+	void Update ()
+    {
+        if (Input.GetButtonDown("Grab"))
+        {
+            Debug.Log("Grab button pushed");
+            if (!hasItem)
+            {
+                Physics2D.queriesStartInColliders = false;
+                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, grabLayerMask);
+                Debug.DrawRay(transform.position, Vector2.right * transform.localScale.x, Color.green);
+                if (hit)
+                {
+                    Debug.Log("Hit something!");
+                    hasItem = true;
+                }
+            }
+            else
+            {
+                hasItem = false;
+                Rigidbody2D itemRigidBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                if (itemRigidBody != null)
+                {
+                    itemRigidBody.velocity = new Vector2(transform.localScale.x, 1) * throwForce;
+                }
+            }
+        }
+
+
+        if (hasItem)
+        {
+            hit.collider.gameObject.transform.position = grabPoint.position;
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position +Vector3.right * transform.localScale.x*distance);
+    }
+}
