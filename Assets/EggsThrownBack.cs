e07fcdbb02eb76ccf015e850a49bd838474515a7
Joecool321 <joecool321@gmail.com>
2017-11-30 19:08:39 -0600

    Updated HUD UI

---
+++ Assets/EggsThrownBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggsThrownBack : MonoBehaviour {
+
+    private Score score;
+
+    Text text;                      // Reference to the Text component.
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set up the reference.
+        text = GetComponent<Text>();
+
+        // Reset the score.
+        score = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<Score>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        text.text = "Eggs Thrown Back: " + score.EggsThrownBack;
+    }
+}
